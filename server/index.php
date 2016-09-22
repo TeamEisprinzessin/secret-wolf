15ba113f8d61ec4c48f16c01f05b132750a412d9
Fabian TheOneAndOnly Meinert <fabianmeinert@posteo.de>
2016-09-22 22:47:20 +0200

    Updated index.php for MySQL-connection, second upload because of human error while commiting :/

--- server/index.php
+++ server/index.php
@@ -15,15 +15,25 @@ if ($conn->connect_error)  {
 	die("Connection failed: " . $conn->connect_error);
 }
 
+// Variables written into MySQL
+$user		= 'testuser';
+$message	= 'How Can Mirrors Be Real If Our Eyes Arent Real';
+// WARNING/To-Do: Problems with Apostrophes etc. for writing into MySQL
 
+// Write into Database
+$sql = "INSERT INTO test (user, message)
+VALUES ('$user', '$message')";
 
+// Check Connection
+if ($conn->query($sql) === TRUE) {
+	echo "New record created successfully";
+} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close Connection
 $conn->close();
 
-
-
-
-
-echo "test";
 
 
 ?>
