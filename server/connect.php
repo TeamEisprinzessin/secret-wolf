4ebce8f5ad867c12b2814887d17eccf8b4c8c58d
kellerkind <kellerkind@guenther>
2016-09-29 10:50:18 +0200

    added connect.php, added rough input boxes for player information

---
+++ server/connect.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+</head>
+
+<body>
+<?php
+
+
+// Create Connection to MySQL-Database
+include "credentials.php";
+
+$gamegamename = $_POST['gamename'];
+$gameusername = $_POST['username'];
+$gameplayernumber = $_POST['playernumber'];
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error)  {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+// Variables written into MySQL
+$user		= 'testuser';
+$message	= 'How Can Mirrors Be Real If Our Eyes Arent Real';
+// WARNING/To-Do: Problems with Apostrophes etc. for writing into MySQL
+
+// Write into Database
+$sql = "INSERT INTO test (user, message)
+VALUES ('$user', '$message')";
+
+// Check Connection
+if ($conn->query($sql) === TRUE) {
+	echo "New record created successfully";
+} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close Connection
+$conn->close();
+
+echo $_POST[''];
+
+?>
+</body>
+
+</html>
+
+
--- server/index.php
+++ server/index.php
@@ -3,41 +3,17 @@
 </head>
 
 <body>
-<?php
-
-
-// Create Connection to MySQL-Database
-include "credentials.php";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error)  {
-	die("Connection failed: " . $conn->connect_error);
-}
-
-// Variables written into MySQL
-$user		= 'testuser';
-$message	= 'How Can Mirrors Be Real If Our Eyes Arent Real';
-// WARNING/To-Do: Problems with Apostrophes etc. for writing into MySQL
-
-// Write into Database
-$sql = "INSERT INTO test (user, message)
-VALUES ('$user', '$message')";
-
-// Check Connection
-if ($conn->query($sql) === TRUE) {
-	echo "New record created successfully";
-} else {
-	echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-// Close Connection
-$conn->close();
 
 
 
-?>
+<form action="connect.php" method="post">
+<p>Dein Spielname: <input type="text" name="gamename"/></p>
+<p>Dein Name: <input type="text" name="username" /></p>
+<p>Deine Spielernummer: <input type="number" name="playernumber" /></p>
+<p> <input type="submit" value="Bestätigen"/></p>
 
+
+</form>
 
 
 </body>
